36b090d5863851980c4360ce0b9c29d256558785
Artem <aprygin@gmail.com>
2018-03-25 18:24:25 +0300

    new

---
+++ lesson1/main.js
@@ -0,0 +1,26 @@
+var budget = prompt('Ваш бюджет?'),
+	shop = prompt('Название вашего магазина');
+
+var	mainList = {};
+mainList = {
+	budget: budget,
+	shop: shop,
+	shopGoods: [],
+	employers: {},
+	open: true
+}
+
+
+for (var i = 0; i < 3; i++) {
+var ask = prompt("Какой тип товаров будем продавать?");
+mainList.shopGoods[i] = ask;
+console.log(mainList.shopGoods);
+};
+
+alert('Ваш бюджет ' + +budget / 30);
+
+// var num = 33721;
+// var digits = num.toString().split('');
+// var multiply = digits[0]*digits[1]*digits[2]*digits[3]*digits[4];
+// console.log(multiply);
+// alert(Math.pow(multiply, 3));
